70c7bbe3a03043df85ccf4aa28cd5f06120d4ed7
Iago92 <rvolobriga@gmail.com>
2016-11-18 02:26:15 +0100

    Add files via upload

--- view/sessions/edit.php
+++ view/sessions/edit.php
@@ -15,27 +15,44 @@ if (!isset($currentuser)){
 ///////////////////////////////////////////////////////////
 ?>
 
-
+<div class="col-md-12 button-buffer">
 <h1><?= i18n("Edit Session")?></h1>
 <div class="col-md-6">
 <form action="index.php?controller=sessions&amp;action=edit&amp;id=<?= $session->getID() ?>" method="POST">
     <div class="form-group">
-      <?= i18n("Name")?>: <input type="text" name="name"	value="<?= $session->getName() ?>" >
+      <label><?= i18n("Name")?>:</label>
+       <input type="text" class="form-control" name="name"	value="<?= $session->getName() ?>" >
+       <div class="help-block">
       <?= isset($errors["name"])?$errors["name"]:"" ?><br>
     </div>
+  </div>
+
     <div class="form-group" >
-      <?= i18n("Description")?>: <input type="text" name="description_session" value="<?= $session->getDescription() ?>">
+      <label><?= i18n("Description")?>:</label>
+       <input type="text" class="form-control" name="description_session" value="<?= $session->getDescription() ?>">
+       <div class="help-block">
       <?= isset($errors["description_session"])?$errors["description_session"]:"" ?><br>
       </div>
+    </div>
+
     <div class="form-group">
-      <?= i18n("Time")?>: <input type="text" name="time" value="<?= $session->getTime() ?>">
+      <label><?= i18n("Time")?>:</label>
+       <input type="text" class="form-control" name="time" value="<?= $session->getTime() ?>">
+       <div class="help-block">
       <?= isset($errors["time"])?$errors["time"]:"" ?><br>
       </div>
+    </div>
+
     <div class="form-group">
-      <?= i18n("Date")?>: <input type="text" name="date" value="<?= $session->getDate() ?>">
+      <label><?= i18n("Date")?>:</label>
+       <input type="text" class="form-control" name="date" value="<?= $session->getDate() ?>">
+       <div class="help-block">
       <?= isset($errors["date"])?$errors["date"]:"" ?><br>
       </div>
-      <input type="submit" name="submit" value= <?= i18n("Add")?> >
+    </div>
+
+      <input type="submit" class="btn btn-info" name="submit" value= <?= i18n("Edit")?> >
 </form>
 
+  </div>
 </div>
